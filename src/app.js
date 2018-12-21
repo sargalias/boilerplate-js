2ace5fd7d848732c262845352f21006cc82bdfbd
Spyros Argalias <sargalias@gmail.com>
2018-12-21 22:13:52 +0000

    Add dynamic imports for code splitting.

--- src/app.js
+++ src/app.js
@@ -3,3 +3,7 @@ import './app.scss';
 const div = document.createElement('div');
 div.innerHTML = 'Hello World';
 document.body.appendChild(div);
+import('path')
+  .then(path => path.join('', ''));
+import('os')
+  .then(os => os.constants);
