86db47a7cef0d17613d59528b3aab66e3ec5cdb3
Spyros Argalias <sargalias@gmail.com>
2019-02-02 22:42:20 +0000

    Format files with prettier.

--- src/app.js
+++ src/app.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './app.scss';
 
-const App = () => (
-  <h1 className={styles.sampleClass}>Hello World!</h1>
-);
+const App = () => <h1 className={styles.sampleClass}>Hello World!</h1>;
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app'),
-);
+ReactDOM.render(<App />, document.getElementById('app'));
 
 export default App;
